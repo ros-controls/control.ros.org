c2329bfdf0e14459b7f04f80b9d6800d31fc06e2
Denis Stogl <denis@stogl.de>
2021-07-09 13:49:29 +0200

    Test and correct paths on local machine. Rename project names to look nicer.

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'control.ros.org'
+project = 'ros-controls'
 copyright = '2021, ros2_control Maintainers'
 author = 'ros2_control Maintainers'
 
@@ -112,12 +112,13 @@ html_context = {
     "conf_py_path": "/",
     "source_suffix": source_suffix,
     "css_files": ['_static/override.css'],
-    "favicon": "logo_ros-controls.png",
+    "favicon": "favicon_ros-controls.ico",
     "logo": "logo_ros-controls.png"
 }
 
-html_logo = "logo_ros-controls.png"
+html_logo = "images/logo_ros-controls.png"
 logo_only = True
+html_favicon = "images/favicon_ros-controls.ico"
 
 github_url = "https://github.com/ros-controls/control.ros.org"
 
