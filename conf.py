43c0952a4775007b965597ea8cbbe1f887d490bd
Manuel <mamueluth@gmail.com>
2022-03-31 10:41:18 +0200

    prepared for merge to master

--- conf.py
+++ conf.py
@@ -34,7 +34,7 @@ copyright = "{}, {}".format(time.strftime("%Y"), author)
 ros_distro = "rolling"
 distro_title = "Rolling"
 distro_title_full = "Rolling Ridley"
-repos_file_branch = "rolling"
+repos_file_branch = "master"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -178,8 +178,8 @@ html_sourcelink_suffix = ""
 
 
 # Add branches you want to whtielist here.
-smv_branch_whitelist = r"^(foxy|galactic|rolling)$"
-smv_released_pattern = r"^refs/(heads|remotes/[^/]+)/(foxy|galactic|rolling).*$"
+smv_branch_whitelist = r"^(foxy|galactic|master)$"
+smv_released_pattern = r"^refs/(heads|remotes/[^/]+)/(foxy|galactic).*$"
 smv_remote_whitelist = r"^(origin)$"
 smv_latest_version = "galactic"
 smv_eol_versions = []
@@ -303,7 +303,7 @@ def smv_rewrite_configs(app, config):
     # to rewrite the various configuration items with the current version.
     if app.config.smv_current_version != "":
         branch_distro = {
-            "rolling": "rolling",
+            "master": "rolling",
             "foxy": "foxy",
             "galactic": "galactic"
         }
