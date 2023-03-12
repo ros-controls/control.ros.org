1c063c858e8155c38982fdf17e88331aa6601377
bmagyar <bmagyar@users.noreply.github.com>
2023-03-12 05:31:45 +0000

    deploy: 75d46712e4226ef49a7a99a2a925b6a48f9f576f

--- master/doc/api/search/all_1.js
+++ master/doc/api/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['bcolors_16',['bcolors',['../classcontroller__manager_1_1spawner_1_1bcolors.html',1,'controller_manager::spawner']]],
+  ['bcolors_16',['bcolors',['../classcontroller__manager_1_1hardware__spawner_1_1bcolors.html',1,'controller_manager.hardware_spawner.bcolors'],['../classcontroller__manager_1_1spawner_1_1bcolors.html',1,'controller_manager.spawner.bcolors']]],
   ['blackboxtest_17',['BlackBoxTest',['../classBlackBoxTest.html',1,'']]]
 ];
