dd7d9e4dfc3a2dee1debb2755af3d386090f7ce5
bmagyar <bmagyar@users.noreply.github.com>
2023-05-01 05:32:22 +0000

    deploy: 311a61a8cd86b215468999fd518f57ad6339e773

--- foxy/_static/documentation_options.js
+++ foxy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Apr 2023',
+    VERSION: 'May 2023',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
