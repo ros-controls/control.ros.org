a1c3a2c919f479c6da9cd45937672ef0a679290f
bmagyar <bmagyar@users.noreply.github.com>
2023-08-22 05:32:41 +0000

    deploy: e451ac0a1cb9eadfd7de2614a6d1301512ebf4b9

--- galactic/_static/documentation_options.js
+++ galactic/_static/documentation_options.js
@@ -1,5 +1,4 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: 'Aug 2023',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
