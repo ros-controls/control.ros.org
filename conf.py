817b97d2cc06f9dd064f0e21c4cc678ffa8b8d29
Dr. Denis <denis@stoglrobotics.de>
2023-12-11 19:25:34 +0100

    Reactive Plausible again (#203)

--- conf.py
+++ conf.py
@@ -121,6 +121,10 @@ html_theme_options = {
     "logo_only": True,
 }
 
+html_js_files = [
+    ('https://plausible.io/js/script.js', {"data-domain": "control.ros.org", "defer": "defer"}),
+]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
