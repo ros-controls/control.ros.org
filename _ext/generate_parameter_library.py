9488b2569039febde11b78747d3cc9c03f1cc7cc
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2023-12-28 13:15:05 +0000

    Add optional "context" for description of structs

--- _ext/generate_parameter_library.py
+++ _ext/generate_parameter_library.py
@@ -2,41 +2,57 @@ from docutils import nodes
 from sphinx.util.docutils import SphinxDirective
 from sphinx.util.nodes import nested_parse_with_titles
 from docutils.statemachine import ViewList
+import yaml
+import re
 from generate_parameter_library_py.parse_yaml import (
     GenerateCode,
 )
 from generate_parameter_library_py.generate_markdown import (
     DefaultConfigMarkdown,
-    ParameterDetailRst,
-    RuntimeParameterDetailRst
+    ParameterDetailMarkdown,
+    RuntimeParameterDetailMarkdown
 )
 
 class GeneraterParameterLibraryDetails(SphinxDirective):
     required_arguments = 1
-    optional_arguments = 0
+    optional_arguments = 1 # context yaml file, "key: string"
 
     def run(self):
         # get the absolute path from sphinx tree
         yaml_file = self.env.relfn2path(self.arguments[0], self.env.docname)[0]
+        if len(self.arguments) > 1:
+          context_yaml_file = self.env.relfn2path(self.arguments[1], self.env.docname)[0]
+          with open(context_yaml_file, 'r') as file:
+            context_yaml_data = yaml.safe_load(file)
+        else:
+          context_yaml_data = {}
 
         gen_param_struct = GenerateCode("rst")
         gen_param_struct.parse(yaml_file, "")
 
         param_details = [
-            ParameterDetailRst(param)
+            ParameterDetailMarkdown(param)
             for param in gen_param_struct.declare_parameters
         ]
         runtime_param_details = [
-            RuntimeParameterDetailRst(param)
+            RuntimeParameterDetailMarkdown(param)
             for param in gen_param_struct.declare_dynamic_parameters
         ]
         param_strings_map = {detail.declare_parameters.parameter_name: str(detail) for detail in param_details}
         param_strings_map.update({detail.declare_parameters.parameter_name: str(detail) for detail in runtime_param_details})
+        # add optional context data from yaml. we don't use a jinja template here -> add the indent manually
+        param_strings_map.update({key: str(key) + "\n" + re.sub(
+                r'(?m)^(?!$)\s*',
+                '  ',
+                str(value),
+                flags=re.MULTILINE,
+            ) + "\n" for key, value in context_yaml_data.items()})
 
         # Sort the keys hierarchically by splitting on '.' and sorting each level
         sorted_keys = sorted(param_strings_map.keys(), key=lambda s: s.split('.'))
 
         docs = "\n".join(param_strings_map[key] for key in sorted_keys)
+        # print(docs)
 
         # Add the content one line at a time.
         # Second argument is the filename to report in any warnings
