e381fa527941d86edca09d5535628cd3ae2d9a68
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-04-20 11:02:00 +0000

    Update exclude patterns

--- conf.py
+++ conf.py
@@ -67,10 +67,13 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-# exclude index.rst files for metapackages for rosdoc2
+# exclude index.rst files from packages/metapackages for rosdoc2
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store",
                     "**/CHANGELOG.rst", "**/README.rst",
                     "doc/ros2_control/ros2_control/**.rst",
+                    "doc/realtime_tools/doc/**.rst",
+                    "doc/control_msgs/doc/**.rst",
+                    "doc/control_toolbox/doc/**.rst",
                     "doc/ros2_controllers/ros2_controllers/**.rst"]
 
 # The name of the Pygments (syntax highlighting) style to use.
