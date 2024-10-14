0243033e491964b98349e47465ee657b969875aa
bmagyar <bmagyar@users.noreply.github.com>
2024-10-14 05:41:12 +0000

    deploy: 31f2c1d9c30aa0d4853d8d9e3c18b7516013dde5

--- humble/doc/api/search/all_2.js
+++ humble/doc/api/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['bcolors_24',['bcolors',['../classcontroller__manager_1_1hardware__spawner_1_1bcolors.html',1,'controller_manager.hardware_spawner.bcolors'],['../classcontroller__manager_1_1spawner_1_1bcolors.html',1,'controller_manager.spawner.bcolors']]],
+  ['bcolors_24',['bcolors',['../classcontroller__manager_1_1controller__manager__services_1_1bcolors.html',1,'controller_manager::controller_manager_services']]],
   ['bicyclesteeringcontroller_25',['BicycleSteeringController',['../classbicycle__steering__controller_1_1BicycleSteeringController.html',1,'bicycle_steering_controller']]]
 ];
