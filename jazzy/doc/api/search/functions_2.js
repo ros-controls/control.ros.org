f1ee89bbd1c05017efa61a386fe2e092dfb06c11
bmagyar <bmagyar@users.noreply.github.com>
2025-04-10 05:42:31 +0000

    deploy: 24db1762d5dce338ee1fe999d44112c73beb7273

--- jazzy/doc/api/search/functions_2.js
+++ jazzy/doc/api/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['bounded_5fpush_0',['bounded_push',['../classrealtime__tools_1_1LockFreeQueueBase.html#a5c7dfa2bb4751ff249b05fccc176804a',1,'realtime_tools::LockFreeQueueBase']]]
+  ['bind_5fcommand_5flimiter_5fto_5finterface_0',['bind_command_limiter_to_interface',['../classhardware__interface_1_1ResourceStorage.html#abae1861bf8f8b0fc904c5f4eac5578ee',1,'hardware_interface::ResourceStorage']]],
+  ['bounded_5fpush_1',['bounded_push',['../classrealtime__tools_1_1LockFreeQueueBase.html#a5c7dfa2bb4751ff249b05fccc176804a',1,'realtime_tools::LockFreeQueueBase']]]
 ];
--- rolling/doc/api/search/all_2.js
+++ rolling/doc/api/search/all_2.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['bcolors_0',['bcolors',['../classcontroller__manager_1_1controller__manager__services_1_1bcolors.html',1,'controller_manager::controller_manager_services']]],
   ['bicyclesteeringcontroller_1',['BicycleSteeringController',['../classbicycle__steering__controller_1_1BicycleSteeringController.html',1,'bicycle_steering_controller']]],
-  ['bounded_5fpush_2',['bounded_push',['../classrealtime__tools_1_1LockFreeQueueBase.html#a5c7dfa2bb4751ff249b05fccc176804a',1,'realtime_tools::LockFreeQueueBase']]]
+  ['bind_5fcommand_5flimiter_5fto_5finterface_2',['bind_command_limiter_to_interface',['../classhardware__interface_1_1ResourceStorage.html#abae1861bf8f8b0fc904c5f4eac5578ee',1,'hardware_interface::ResourceStorage']]],
+  ['bounded_5fpush_3',['bounded_push',['../classrealtime__tools_1_1LockFreeQueueBase.html#a5c7dfa2bb4751ff249b05fccc176804a',1,'realtime_tools::LockFreeQueueBase']]]
 ];
