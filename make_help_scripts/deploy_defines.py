9638e49ce2e05f20f6557a57a4c883ec810ff8d7
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2025-07-01 23:05:29 +0200

    Add rt_tools and control_tb to PR workflow (#470)
    (cherry picked from commit cb7a42f76cc88bb90088fa4b62a707d36c6611da)

--- make_help_scripts/deploy_defines.py
+++ make_help_scripts/deploy_defines.py
@@ -140,7 +140,7 @@ repos = {
             "jazzy": "ros2-master",
             "rolling": "ros2-master"
         },
-        "pr": None
+        "pr": os.environ.get('CONTROL_TOOLBOX_PR')
     },
     "control_msgs": {
         "url": "https://github.com/ros-controls/control_msgs",
@@ -164,7 +164,7 @@ repos = {
             "jazzy": "jazzy",
             "rolling": "master"
         },
-        "pr": None
+        "pr": os.environ.get('REALTIME_TOOLS_PR')
     },
     "kinematics_interface": {
         "url": "https://github.com/ros-controls/kinematics_interface",
