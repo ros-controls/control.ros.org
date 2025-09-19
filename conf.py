700599bc2d9bfc7fa202b191dd354a2a71179599
jsantoso <jsantoso@wpi.edu>
2025-09-19 14:48:24 -0500

    add to linkcheck_ignore instead

--- conf.py
+++ conf.py
@@ -178,12 +178,12 @@ github_url = "https://github.com/ros-controls/control.ros.org"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#filtering
 linkcheck_anchors_ignore_for_url = [
     'https://github.com/',
-    'https://index.ros.org/',
-    'https://blender.org/'
+    'https://index.ros.org/'
     ]
 linkcheck_ignore = [
     r'https://gazebosim.org/home',
-    r'https://blogs.oracle.com/linux/post/task-priority'
+    r'https://blogs.oracle.com/linux/post/task-priority',
+    r'https://www.blender.org/'
 ]
 
 # -- Options for HTMLHelp output ---------------------------------------------
