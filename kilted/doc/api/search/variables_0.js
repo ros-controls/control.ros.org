d1b3ee60bd43cade6766eb442666a84e1706ed18
bmagyar <bmagyar@users.noreply.github.com>
2025-09-28 05:41:45 +0000

    deploy: 1c7b54ca72e7e15ba4cc3a814f42dc855c7e13a9

--- kilted/doc/api/search/variables_0.js
+++ kilted/doc/api/search/variables_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['activate_5fall_0',['activate_all',['../structhardware__interface_1_1ResourceManagerParams.html#a32fd4f5d2fc60050182eddc227f51d69',1,'hardware_interface::ResourceManagerParams']]],
-  ['antiwindup_5fstrat_5f_1',['antiwindup_strat_',['../structcontrol__toolbox_1_1Pid_1_1Gains.html#a5a18e7a6b1e7fd9c3bfc0084b72e481a',1,'control_toolbox::Pid::Gains']]],
-  ['available_5fstate_5finterfaces_5f_2',['available_state_interfaces_',['../classhardware__interface_1_1ResourceStorage.html#a21fe95fdf58b2c51706f9bc347ab24f4',1,'hardware_interface::ResourceStorage']]]
+  ['allow_5fcontroller_5factivation_5fwith_5finactive_5fhardware_1',['allow_controller_activation_with_inactive_hardware',['../structhardware__interface_1_1ResourceManagerParams.html#a4468ee93af7ed50a4a4c3da91264b8b2',1,'hardware_interface::ResourceManagerParams']]],
+  ['antiwindup_5fstrat_5f_2',['antiwindup_strat_',['../structcontrol__toolbox_1_1Pid_1_1Gains.html#a5a18e7a6b1e7fd9c3bfc0084b72e481a',1,'control_toolbox::Pid::Gains']]],
+  ['available_5fstate_5finterfaces_5f_3',['available_state_interfaces_',['../classhardware__interface_1_1ResourceStorage.html#a21fe95fdf58b2c51706f9bc347ab24f4',1,'hardware_interface::ResourceStorage']]]
 ];
