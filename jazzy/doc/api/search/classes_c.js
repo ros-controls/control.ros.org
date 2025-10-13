2d0560a8bc2df1eefccb1d1172783a29e4a770bb
bmagyar <bmagyar@users.noreply.github.com>
2025-10-13 05:45:18 +0000

    deploy: 2b631b4450c2eacd5ae05b24a1b4dbfea9908eca

--- jazzy/doc/api/search/classes_c.js
+++ jazzy/doc/api/search/classes_c.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['mecanumdrivecontroller_0',['MecanumDriveController',['../classmecanum__drive__controller_1_1MecanumDriveController.html',1,'mecanum_drive_controller']]],
-  ['mimicjoint_1',['mimicjoint',['../structhardware__interface_1_1MimicJoint.html',1,'hardware_interface::MimicJoint'],['../structMimicJoint.html',1,'MimicJoint']]],
-  ['movingaveragestatistics_2',['MovingAverageStatistics',['../classros2__control_1_1MovingAverageStatistics.html',1,'ros2_control']]],
-  ['movingaveragestatisticsdata_3',['MovingAverageStatisticsData',['../classros2__control_1_1MovingAverageStatisticsData.html',1,'ros2_control']]],
-  ['multiinterfaceforwardcommandcontroller_4',['MultiInterfaceForwardCommandController',['../classforward__command__controller_1_1MultiInterfaceForwardCommandController.html',1,'forward_command_controller']]],
-  ['mutex_5',['mutex',['../classrealtime__tools_1_1detail_1_1mutex.html',1,'realtime_tools::detail']]]
+  ['magneticfieldsensor_0',['MagneticFieldSensor',['../classsemantic__components_1_1MagneticFieldSensor.html',1,'semantic_components']]],
+  ['mecanumdrivecontroller_1',['MecanumDriveController',['../classmecanum__drive__controller_1_1MecanumDriveController.html',1,'mecanum_drive_controller']]],
+  ['mimicjoint_2',['mimicjoint',['../structhardware__interface_1_1MimicJoint.html',1,'hardware_interface::MimicJoint'],['../structMimicJoint.html',1,'MimicJoint']]],
+  ['movingaveragestatistics_3',['MovingAverageStatistics',['../classros2__control_1_1MovingAverageStatistics.html',1,'ros2_control']]],
+  ['movingaveragestatisticsdata_4',['MovingAverageStatisticsData',['../classros2__control_1_1MovingAverageStatisticsData.html',1,'ros2_control']]],
+  ['multiinterfaceforwardcommandcontroller_5',['MultiInterfaceForwardCommandController',['../classforward__command__controller_1_1MultiInterfaceForwardCommandController.html',1,'forward_command_controller']]],
+  ['mutex_6',['mutex',['../classrealtime__tools_1_1detail_1_1mutex.html',1,'realtime_tools::detail']]]
 ];
