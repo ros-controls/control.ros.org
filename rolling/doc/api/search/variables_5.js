28b336df9d922a4ac68cafa267484ada73db03db
bmagyar <bmagyar@users.noreply.github.com>
2025-10-16 05:43:20 +0000

    deploy: 79b509d512b6946821426383ced4e6d248e36eae

--- rolling/doc/api/search/variables_5.js
+++ rolling/doc/api/search/variables_5.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['goal_5fstate_5ftolerance_0',['goal_state_tolerance',['../structjoint__trajectory__controller_1_1SegmentTolerances.html#a19a6448780b5bd1caa06f2b717a4ee8b',1,'joint_trajectory_controller::SegmentTolerances']]],
   ['goal_5ftime_5ftolerance_1',['goal_time_tolerance',['../structjoint__trajectory__controller_1_1SegmentTolerances.html#ae887dc9e130600f92664a40516c1d94f',1,'joint_trajectory_controller::SegmentTolerances']]],
-  ['gpio_5fmock_5fcommand_5fvalues_5f_2',['gpio_mock_command_values_',['../classmock__components_1_1GenericSystem.html#aada33cd190f8d03f9dbb31b22e380fd9',1,'mock_components::GenericSystem']]],
-  ['gpios_3',['gpios',['../structhardware__interface_1_1HardwareInfo.html#aaba1f315215e7425123b75ffcb87b5ce',1,'hardware_interface::HardwareInfo']]],
-  ['group_4',['group',['../structhardware__interface_1_1HardwareComponentInfo.html#a4cf36a1188216aeebdd5723b1832d7fe',1,'hardware_interface::HardwareComponentInfo::group'],['../structhardware__interface_1_1HardwareInfo.html#a6b88e56e222f5dacb9b343fe1d0802a2',1,'hardware_interface::HardwareInfo::group']]]
+  ['gpios_2',['gpios',['../structhardware__interface_1_1HardwareInfo.html#aaba1f315215e7425123b75ffcb87b5ce',1,'hardware_interface::HardwareInfo']]],
+  ['group_3',['group',['../structhardware__interface_1_1HardwareComponentInfo.html#a4cf36a1188216aeebdd5723b1832d7fe',1,'hardware_interface::HardwareComponentInfo::group'],['../structhardware__interface_1_1HardwareInfo.html#a6b88e56e222f5dacb9b343fe1d0802a2',1,'hardware_interface::HardwareInfo::group']]]
 ];
