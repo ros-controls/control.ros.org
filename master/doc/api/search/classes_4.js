9db8dbf46ad161f86dfef9f8c1f8239acbeea062
bmagyar <bmagyar@users.noreply.github.com>
2025-11-11 05:44:17 +0000

    deploy: 75dd7657ca5882534cc06fe16b84269f3c148c4d

--- master/doc/api/search/classes_4.js
+++ master/doc/api/search/classes_4.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['error_5fmutex_5ftype_5ft_0',['error_mutex_type_t',['../structrealtime__tools_1_1detail_1_1error__mutex__type__t.html',1,'realtime_tools::detail']]],
   ['exception_1',['Exception',['../classtransmission__interface_1_1Exception.html',1,'transmission_interface']]],
-  ['exponentialfilter_2',['ExponentialFilter',['../classcontrol__filters_1_1ExponentialFilter.html',1,'control_filters']]],
+  ['exponentialfilter_2',['exponentialfilter',['../classcontrol__filters_1_1ExponentialFilter.html',1,'control_filters::ExponentialFilter&lt; T &gt;'],['../classcontrol__toolbox_1_1ExponentialFilter.html',1,'control_toolbox::ExponentialFilter&lt; T &gt;']]],
   ['externalrrbotforcetorquesensorhardware_3',['ExternalRRBotForceTorqueSensorHardware',['../classros2__control__demo__example__5_1_1ExternalRRBotForceTorqueSensorHardware.html',1,'ros2_control_demo_example_5']]]
 ];
