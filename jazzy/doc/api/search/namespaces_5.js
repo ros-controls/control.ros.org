bd4a0ffa599050a8d84cd4e8f23d8f882de8aa78
bmagyar <bmagyar@users.noreply.github.com>
2025-12-05 05:46:39 +0000

    deploy: 37972e9cbe8cbcded8673d24517d56e597feb960

--- jazzy/doc/api/search/namespaces_5.js
+++ jazzy/doc/api/search/namespaces_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['kinematics_5finterface_0',['kinematics_interface',['../namespacekinematics__interface.html',1,'']]],
-  ['kinematics_5finterface_5fkdl_1',['kinematics_interface_kdl',['../namespacekinematics__interface__kdl.html',1,'']]]
+  ['kinematics_5finterface_5fkdl_1',['kinematics_interface_kdl',['../namespacekinematics__interface__kdl.html',1,'']]],
+  ['kinematics_5finterface_5fpinocchio_2',['kinematics_interface_pinocchio',['../namespacekinematics__interface__pinocchio.html',1,'']]]
 ];
--- kilted/doc/api/search/all_b.js
+++ kilted/doc/api/search/all_b.js
@@ -3,6 +3,8 @@ var searchData=
   ['kilted_0',['ros2_control - kilted',['../index.html',1,'']]],
   ['kinematics_5finterface_1',['kinematics_interface',['../namespacekinematics__interface.html',1,'']]],
   ['kinematics_5finterface_5fkdl_2',['kinematics_interface_kdl',['../namespacekinematics__interface__kdl.html',1,'']]],
-  ['kinematicsinterface_3',['KinematicsInterface',['../classkinematics__interface_1_1KinematicsInterface.html',1,'kinematics_interface']]],
-  ['kinematicsinterfacekdl_4',['KinematicsInterfaceKDL',['../classkinematics__interface__kdl_1_1KinematicsInterfaceKDL.html',1,'kinematics_interface_kdl']]]
+  ['kinematics_5finterface_5fpinocchio_3',['kinematics_interface_pinocchio',['../namespacekinematics__interface__pinocchio.html',1,'']]],
+  ['kinematicsinterface_4',['KinematicsInterface',['../classkinematics__interface_1_1KinematicsInterface.html',1,'kinematics_interface']]],
+  ['kinematicsinterfacekdl_5',['KinematicsInterfaceKDL',['../classkinematics__interface__kdl_1_1KinematicsInterfaceKDL.html',1,'kinematics_interface_kdl']]],
+  ['kinematicsinterfacepinocchio_6',['KinematicsInterfacePinocchio',['../classkinematics__interface__pinocchio_1_1KinematicsInterfacePinocchio.html',1,'kinematics_interface_pinocchio']]]
 ];
