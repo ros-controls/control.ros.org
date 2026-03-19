2af156762123cf2f18919337a24cd556c5100ed7
bmagyar <bmagyar@users.noreply.github.com>
2026-03-19 06:21:12 +0000

    deploy: 8efef006591298b8c095ba27cf352eb5df1ae150

--- humble/doc/api/search/variables_7.js
+++ humble/doc/api/search/variables_7.js
@@ -4,10 +4,11 @@ var searchData=
   ['i_5fgain_5f_1',['i_gain_',['../structcontrol__toolbox_1_1Pid_1_1Gains.html#a5682fcd3a75dcfdf7f242522f702444b',1,'control_toolbox::Pid::Gains']]],
   ['i_5fmax_5f_2',['i_max_',['../structcontrol__toolbox_1_1Pid_1_1Gains.html#aea997696600eb3bd5449724faa2c0502',1,'control_toolbox::Pid::Gains']]],
   ['i_5fmin_5f_3',['i_min_',['../structcontrol__toolbox_1_1Pid_1_1Gains.html#a26a4c582a7de6d94fc9c924a074d5423',1,'control_toolbox::Pid::Gains']]],
-  ['imu_5fsensor_5fdata_5f_4',['imu_sensor_data_',['../classgazebo__ros2__control_1_1GazeboSystemPrivate.html#a5403b56bcab107df7384740779466c8b',1,'gazebo_ros2_control::GazeboSystemPrivate::imu_sensor_data_'],['../classImuData.html#aea5660e0ba8060bf1fc8e3ec8bb17084',1,'ImuData::imu_sensor_data_']]],
-  ['imus_5f_5',['imus_',['../classgz__ros2__control_1_1GazeboSimSystemPrivate.html#a0d8bc1a400ca3eae8bd33d0d40505a0d',1,'gz_ros2_control::GazeboSimSystemPrivate']]],
-  ['initial_5fvalue_6',['initial_value',['../structhardware__interface_1_1InterfaceInfo.html#a1681ac364efe0a5961addbe90d10edfb',1,'hardware_interface::InterfaceInfo']]],
-  ['interpolation_5fmethod_5f_7',['interpolation_method_',['../classjoint__trajectory__controller_1_1JointTrajectoryController.html#a0fa999d67e7b56da07423350905d61e8',1,'joint_trajectory_controller::JointTrajectoryController']]],
-  ['is_5factuated_8',['is_actuated',['../structjointData.html#a7e7f333e09baec3c97cb3180f68d0531',1,'jointData']]],
-  ['is_5fjoint_5factuated_5f_9',['is_joint_actuated_',['../classgazebo__ros2__control_1_1GazeboSystemPrivate.html#a8e11d6d462ec6496d8e682fe2260a63c',1,'gazebo_ros2_control::GazeboSystemPrivate']]]
+  ['if_5fname_5fposition_4',['if_name_position',['../structjointData.html#aaf563a42722c12318f91136ba269af68',1,'jointData']]],
+  ['imu_5fsensor_5fdata_5f_5',['imu_sensor_data_',['../classgazebo__ros2__control_1_1GazeboSystemPrivate.html#a5403b56bcab107df7384740779466c8b',1,'gazebo_ros2_control::GazeboSystemPrivate::imu_sensor_data_'],['../classImuData.html#aea5660e0ba8060bf1fc8e3ec8bb17084',1,'ImuData::imu_sensor_data_']]],
+  ['imus_5f_6',['imus_',['../classgz__ros2__control_1_1GazeboSimSystemPrivate.html#a0d8bc1a400ca3eae8bd33d0d40505a0d',1,'gz_ros2_control::GazeboSimSystemPrivate']]],
+  ['initial_5fvalue_7',['initial_value',['../structhardware__interface_1_1InterfaceInfo.html#a1681ac364efe0a5961addbe90d10edfb',1,'hardware_interface::InterfaceInfo']]],
+  ['interpolation_5fmethod_5f_8',['interpolation_method_',['../classjoint__trajectory__controller_1_1JointTrajectoryController.html#a0fa999d67e7b56da07423350905d61e8',1,'joint_trajectory_controller::JointTrajectoryController']]],
+  ['is_5factuated_9',['is_actuated',['../structjointData.html#a7e7f333e09baec3c97cb3180f68d0531',1,'jointData']]],
+  ['is_5fjoint_5factuated_5f_10',['is_joint_actuated_',['../classgazebo__ros2__control_1_1GazeboSystemPrivate.html#a8e11d6d462ec6496d8e682fe2260a63c',1,'gazebo_ros2_control::GazeboSystemPrivate']]]
 ];
