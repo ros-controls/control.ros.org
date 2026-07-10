5d700386ba20b432ba92141e6003d9772bcc71d7
Ishan Pathak <148351455+Ishan1923@users.noreply.github.com>
2026-07-10 17:02:43 +0530

    Add llms.txt setup (#664)

--- conf.py
+++ conf.py
@@ -143,6 +143,8 @@ html_js_files = [
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_extra_path = ['_static/llms.txt']
+
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
