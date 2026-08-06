980104ce7ad9e6127f520f2aa3272a798a859021
bmagyar <bmagyar@users.noreply.github.com>
2026-08-06 06:30:23 +0000

    deploy: 87ba54b19740d6c1112fe0387119014d4cb36756

--- lyrical/doc/api/search/classes_15.js
+++ lyrical/doc/api/search/classes_15.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['zenbedded_5fcommand_5ft_0',['zenbedded_command_t',['../structzenbedded__command__t.html',1,'']]],
-  ['zenbedded_5fstate_5ft_1',['zenbedded_state_t',['../structzenbedded__state__t.html',1,'']]],
-  ['zenbeddedclient_2',['ZenbeddedClient',['../classZenbeddedClient.html',1,'']]],
-  ['zenbeddedhardware_3',['ZenbeddedHardware',['../classzenbedded_1_1ZenbeddedHardware.html',1,'zenbedded']]]
+  ['zenbeddedclient_0',['ZenbeddedClient',['../classZenbeddedClient.html',1,'']]],
+  ['zenbeddedhardware_1',['ZenbeddedHardware',['../classzenbedded_1_1ZenbeddedHardware.html',1,'zenbedded']]]
 ];
