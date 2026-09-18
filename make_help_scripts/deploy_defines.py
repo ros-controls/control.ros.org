cec34c1a0bab2bb01bc54aaa6301624673d22f06
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2026-09-18 03:17:50 +0200

    Add more hooks for CI workflows (#728)
    * Add more hooks for CI workflows
    
    * Use job context workflow
    
    https://docs.github.com/en/actions/reference/workflows-and-actions/contexts#example-usage-of-job-context-workflow-identity

--- make_help_scripts/deploy_defines.py
+++ make_help_scripts/deploy_defines.py
@@ -165,7 +165,7 @@ repos = {
             "lyrical": "master",
             "rolling": "master"
         },
-        "pr": None
+        "pr": os.environ.get('CONTROL_MSGS_PR')
     },
     "realtime_tools": {
         "url": "https://github.com/ros-controls/realtime_tools",
@@ -193,7 +193,7 @@ repos = {
             "lyrical": "master",
             "rolling": "master"
         },
-        "pr": None
+        "pr": os.environ.get('KINEMATICS_INTERFACE_PR')
     },
     "topic_based_hardware_interfaces": {
         "url": "https://github.com/ros-controls/topic_based_hardware_interfaces",
@@ -204,7 +204,7 @@ repos = {
             "lyrical": "main",
             "rolling": "main"
         },
-        "pr": None
+        "pr": os.environ.get('TOPIC_BASED_HARDWARE_INTERFACES_PR')
     },
     ".github": {
         "url": "https://github.com/ros-controls/.github",
@@ -218,7 +218,7 @@ repos = {
             "lyrical": "master",
             "rolling": "master"
         },
-        "pr": None
+        "pr": os.environ.get('ROS_CONTROLS_GITHUB_PR')
     },
     "mujoco_ros2_control": {
         "url": "https://github.com/ros-controls/mujoco_ros2_control.git",
@@ -229,7 +229,7 @@ repos = {
             "lyrical": "main",
             "rolling": "main"
         },
-        "pr": None
+        "pr": os.environ.get('MUJOCO_ROS2_CONTROL_PR')
     },
     "zephyr-zenoh-integration": {
         "url": "https://github.com/ros-controls/zephyr-zenoh-integration",
@@ -243,6 +243,6 @@ repos = {
             "lyrical": "master",
             "rolling": "master"
         },
-        "pr": None
+        "pr": os.environ.get('ZEPHYR_ZENOH_INTEGRATION_PR')
     }
 }
